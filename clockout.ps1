364c79a11e408114a1f275f566413ed33eee1eea
Zwel Pai <lozberipie@gmail.com>
2022-12-02 18:44:08 +0700

    Fix bugs, add whitespaces

--- clockout.ps1
+++ clockout.ps1
@@ -6,13 +6,13 @@ $t = Read-Host -Prompt "Execution time (24 hr)"
 $rand = Read-Host -Prompt "Randomize minutes?"
 $b = Read-Host -Prompt "Run headed mode?"
 
-$work = if ($w) { "-w " + "`"{0}`"" -f $w } else { "" }
-$proj = if ($p) { "-p " + "`"{0}`"" -f $p } else { "" }
+$work = if ($w) { "-w " + "`"{0}`" " -f $w } else { "" }
+$proj = if ($p) { "-p " + "`"{0}`" " -f $p } else { "" }
 $dur = if ($d) { "-d " + $d } else { "" }
-$submit = if ($s -eq "y" -or $s -eq "Y") { "-s" } else { "" }
-$exc_time = if ($t) { $t } else { 18 }
-$randomize = if ($rand) { $true } else { $false }
-$use_browser = if ($b -eq "y" -or $s -eq "Y") { "-b" } else { "" }
+$submit = if ($s -eq "y" -or $s -eq "Y") { "-s " } else { "" }
+$exc_time = if ($t -and $t -ge 0 -and $t -lt 24) { $t } else { 18 }
+$randomize = if ($rand -eq "y" -or $rand -eq "Y") { $true } else { $false }
+$use_browser = if ($b -eq "y" -or $b -eq "Y") { "-b" } else { "" }
 
 $action = New-ScheduledTaskAction -Execute "path/to/conda.bat" -Argument (
     "run " +
